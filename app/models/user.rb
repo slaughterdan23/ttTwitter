7521b88563dab7e89b59c6c8fa80eb9e59bf9cf0
Daniel Slaughter <slaughter.dan23@gmail.com>
2017-05-16 11:18:50 -0700

    Got the basic logic, username, tweets functioning

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  serialize :following, Array
+
+  has_many :tweets
+
+  validates :username, presence:true
+  validates :username, uniqueness:true
+
+end
