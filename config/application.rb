6cfdfba279019026ed529cb415dd46c7e0c75812
Daniel Slaughter <slaughter.dan23@gmail.com>
2017-05-17 08:15:24 -0700

    Carrierwave setup for feed and show

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.autoload_paths += %W(#{config.root}/app/uploaders)
+# config.autoload_paths += %W(#{config.root}/app/uploaders)
 
 
 module TtTwitter
